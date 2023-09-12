3a9162ca835fe60cac33370c57cc87b6945a8227
anhnguyen <baanh9343@gmail.com>
2023-09-12 21:37:58 +0700

    first commit

---
+++ while.cs
@@ -0,0 +1,14 @@
+internal class Abc
+{
+    
+    private static void Mainxxx(string[] args)
+    {
+        int i = 0;
+        while (i < 10){
+
+            Console.WriteLine(i);
+            i++;
+        }
+
+    }
+}
